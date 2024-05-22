4793e4c6891d1dd9f94b2fb1294221297f378acb
Alexsandro <sysbietec@gmail.com>
2024-05-22 05:47:58 -0300

    atualizados posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,54 +4,63 @@ import styles from './Post.module.css';
 
 
 export function Post({ author, content, id, publishedAt, role }) {
-     console.log(content)
+   function formatDate(date){
+    return new Date(date).toLocaleDateString('pt-BR', {
+        day:'2-digit',
+        month:'long',
+        year:'numeric',
+        weekday:'long',
+        hour:'2-digit',
+        minute:'2-digit',
+        // second:'2-digit',
+        timeZone:'America/Sao_Paulo',
+        hour12:false
+    })
+   }
     return (
-         
-            <article className={styles.post}>
-                <header>
 
-                    <div className={styles.author}>
-                        <Avatar src={author.avatarUrl}/>
-                        <div className={styles.info}>
-                            <strong>{author.name}</strong>
-                            <span>{author.role}</span>
-                        </div>
+        <article className={styles.post}>
+        
+            <header>
+
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl} />
+                    <div className={styles.info}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
-                    <time 
+                </div>
+                <time
                     title='08 de maio as 1:19'
                     dateTime={publishedAt}>
-                         {new Date(publishedAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          })}
-                    </time>
-                </header>
+                    {formatDate(publishedAt)}
+                </time>
+            </header>
 
-                <div className={styles.content}>
-                   {content.map((paragraph, index) =>(
+            <div className={styles.content}>
+                {content.map((paragraph, index) => (
                     <p key={index}>{paragraph.content}</p>
-                   ))}
-                </div>
+                ))}
+            </div>
 
-                <form className={styles.commentForm}>
-                    <strong>Deixe seu feedback</strong>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
 
-                    <textarea 
-                        placeholder='deixe um comentário'
-                    />
+                <textarea
+                    placeholder='deixe um comentário'
+                />
 
-                   <footer>
-                   <button type='submit'>Publicar</button>
-                   </footer>
-                </form>
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
 
-                <div className={styles.commentList}>
-                    <Comment />
-                    <Comment />
-                    <Comment />
-                </div>
-            </article>
-      
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+
     )
 }
